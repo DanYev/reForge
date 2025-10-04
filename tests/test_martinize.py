8e0f17626126498d94d5c59c051dfdd9cbfa7b39
Danis Yangaliev <dyangali@asu.edu>
2025-10-04 10:07:20 -0700

    works

--- tests/test_martinize.py
+++ tests/test_martinize.py
@@ -17,18 +17,27 @@ import pytest
 import shutil
 from reforge.mdsystem.gmxmd import GmxSystem, GmxRun
 
+# Global variables for tests
 mdsys = GmxSystem("tests", "test_sys")
 mdrun = GmxRun("tests", "test_sys", "test_run")
 protein_pdb = mdsys.root / ".." / "1btl.pdb"
 rna_pdb = mdsys.root / ".." / "dsRNA.pdb"
-if mdsys.root.exists():
-    shutil.rmtree(mdsys.root)
+
+@pytest.fixture(scope="module", autouse=True)
+def cleanup_test_files():
+    """Setup and cleanup test files"""
+    # Setup
+    mdsys = GmxSystem("tests", "test_sys")
+    if mdsys.root.exists():
+        shutil.rmtree(mdsys.root)
+    yield mdsys  # This runs the tests
+    shutil.rmtree(mdsys.root) # Cleanup - only runs if tests complete successfully
 
 def test_prepare():
     mdsys.prepare_files(pour_martini=True)
     mdsys.clean_pdb_mm(protein_pdb, add_missing_atoms=True, add_hydrogens=True, pH=7.0)
     mdsys.split_chains()
-    assert (Path(mdsys.topdir) / "chain_A.itp").exists()
+    assert (Path(mdsys.prodir) / "chain_A.pdb").exists()
 
 def test_martini_en():
     mdsys.martinize_proteins_en(ef=700, el=0.0, eu=0.9, p='backbone', pf=500, append=False)
@@ -50,6 +59,3 @@ def test_make_cg_structure():
 def test_make_cg_topology():
     mdsys.make_cg_topology()
     assert mdsys.systop.exists()
-
-if __name__ == '__main__':
-    pytest.main([str(Path(__file__).resolve())])
