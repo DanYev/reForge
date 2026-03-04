d1c90f1d12b7eba8a8c6c5b2ffcafcbc2f609773
Dan Y <dyangali@asu.edu>
2026-03-04 10:39:35 -0700

    passed

--- tests/test_martinize.py
+++ tests/test_martinize.py
@@ -41,13 +41,13 @@ def test_prepare():
     mdsys.split_chains()
     assert (Path(mdsys.prodir) / "chain_A.pdb").exists()
 
-# def test_martini_en():
-#     mdsys.martinize_proteins_en(ef=700, el=0.0, eu=0.9, p='backbone', pf=500, append=False)
-#     assert (Path(mdsys.topdir) / "chain_A.itp").exists()
+def test_martini_en():
+    mdsys.martinize_proteins_en(ef=700, el=0.0, eu=0.9, p='backbone', pf=500, append=False)
+    assert (Path(mdsys.topdir) / "chain_A.itp").exists()
 
-# def test_martini_go():
-#     mdsys.martinize_proteins_go(go_eps=12.0, go_low=0.3, go_up=1.1, p="backbone", pf=500, append=False)
-#     assert (Path(mdsys.topdir) / "chain_A.itp").exists()
+def test_martini_go():
+    mdsys.martinize_proteins_go(go_eps=12.0, go_low=0.3, go_up=1.1, p="backbone", pf=500, append=False)
+    assert (Path(mdsys.topdir) / "chain_A.itp").exists()
 
 def test_martinize_rna():
     shutil.copy(rna_pdb, mdsys.nucdir / "chain_AB.pdb")
